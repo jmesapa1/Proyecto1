32258b6047b9a6a3b41e8f434e6614e302bfba74
julian <julian_andres2323@hotmail.com>
2019-02-12 00:56:36 -0500

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDbWDOpY0kJjy9ZHevTKDgkNqOalQKG-xo",
+    authDomain: "proyecto1-187f2.firebaseapp.com",
+    databaseURL: "https://proyecto1-187f2.firebaseio.com",
+    projectId: "proyecto1-187f2",
+    storageBucket: "proyecto1-187f2.appspot.com",
+    messagingSenderId: "222969532649"
+  }
+};
